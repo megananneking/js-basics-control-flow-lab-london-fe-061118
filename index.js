90c97dd985028679432ca05a1a6707bb5bcba752
megananneking <megan.king@wework.com>
2018-07-25 17:21:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+describe('index.js', function () {
+  describe('scuberGreetingForFeet()', function () {
+    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+    });
+ 
+
+
+
 scuberGreetingForFeet(
   )
+  
+  });
+});
