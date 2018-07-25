ba74889cf631885ac4aca88930c986ef086c4296
megananneking <megan.king@wework.com>
2018-07-25 17:48:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function scuberGreetingForFeet(someNumber) {
   if (someNumber < 400) {
     fare = "This one is on me!"
   }
-  if (someNumber < 2000) {
+  else if (someNumber < 2000) {
     fare = "I will gladly take your thirty bucks."
   }
-  if (someNumber < 2500) {
+  else if (someNumber < 2500) {
     fare = "No can do."
   }
   return fare
