3bcd1961adc9e1ae901112cd740da6d38ee2c429
megananneking <megan.king@wework.com>
2018-07-25 17:42:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function scuberGreetingForFeet(someNumber) {
   if (scuberGreetingForFeet(199)).to.equal('This one is on me!') {
+  if (scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.')
   }
-  return result
-}
