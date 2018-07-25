4d50e0e0ba68d29b4e830666cc46495d9edf095e
megananneking <megan.king@wework.com>
2018-07-25 18:06:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function ternaryCheckCity(city) {
   if (city = 'NYC') {
     response = "Ok, sounds good."
   }
-  else if (city == 'NYC') {
+  else if (city !== 'NYC') {
     response = "No go."
   }
   return response
